f981ebfb665d2c73e6b50cc9c842721f9605540e
fahedelkhatib <felkh002@ucr.edu>
2020-01-22 15:59:57 -0800

    gotta dip

--- sampleredditstream.py
+++ sampleredditstream.py
@@ -16,9 +16,13 @@ print('okay so you got this far')
 
 loops = 0
 subreddit = reddit.subreddit('all')
+print('PRINTING POST STREAM FROM SUBREDDIT: r/' + subreddit.display_name + '\n\n')
+#print(subreddit.display_name)
+
 for submission in subreddit.stream.submissions(skip_existing=True):
     loops = loops + 1
     print(str(submission.title.encode('utf-8')))
+    #print(str(submission.description.encode('utf-8')))
     #print(str(comment))
     time.sleep(0.5)
 
