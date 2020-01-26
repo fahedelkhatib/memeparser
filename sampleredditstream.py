18367953ac14d177f8ceb70839c1b9dab2b15aa4
Fahed Elkhatib <felkh002@ucr.edu>
2020-01-26 15:59:14 -0800

    hey ruth guess what

--- sampleredditstream.py
+++ sampleredditstream.py
@@ -1,5 +1,7 @@
 import praw
 import time
+import os
+
 
 def initializeRedditInstance():
     redditObject = praw.Reddit(client_id='LJ2JgRga7CP6Cw',
@@ -10,18 +12,24 @@ def initializeRedditInstance():
 
 reddit = initializeRedditInstance()
 
+os.system('cls')
+
 
 print('okay so you got this far')
-
+subredditname = input('so then... tell me which subreddit you want to monitor :)\n')
 
 loops = 0
-subreddit = reddit.subreddit('all')
+subreddit = reddit.subreddit(subredditname)
 print('PRINTING POST STREAM FROM SUBREDDIT: r/' + subreddit.display_name + '\n\n')
 #print(subreddit.display_name)
 
 for submission in subreddit.stream.submissions(skip_existing=True):
     loops = loops + 1
+    print('******************************************************')
     print(str(submission.title.encode('utf-8')))
+    print(str(submission.url.encode('utf-8')))
+    print('******************************************************')
+    
     #print(str(submission.description.encode('utf-8')))
     #print(str(comment))
     time.sleep(0.5)
