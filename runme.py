38e47fa0674dabeb4a6c75d3a9a40ae70baff9ec
fahedelkhatib <felkh002@ucr.edu>
2020-02-10 22:08:43 -0800

    cleaned up code, made most of code into functions that can be duplicated/reused for other websites if needed

---
+++ runme.py
@@ -0,0 +1,40 @@
+import functions
+
+showDebugMessages = False
+
+reddit = functions.initializeRedditInstance()
+homedirectory = functions.os.getcwd()
+debugLogFileName = 'C:\\users\\pc\\documents\\memeparser\\debuglog.txt'
+
+functions.os.system('cls')
+print('okay, so you got this far')
+subredditname = input('so then... tell me which subreddit you want to monitor, my precious :)\n')
+subredditname = 'nukedmemes+dankmemes+memes+deepfriedmemes'
+bVerifySubreddit = input("Are you sure? I don't judge! (answer True or False)\n")
+bVerifySubreddit = 't'
+
+#while loop to verify subreddit name inputted
+while((bVerifySubreddit != "True") &
+      (bVerifySubreddit != "true") &
+      (bVerifySubreddit != "T") &
+      (bVerifySubreddit != "t") &
+      (bVerifySubreddit != "y") &
+      (bVerifySubreddit != "Yes") &
+      (bVerifySubreddit != "yes")):
+    subredditname = input("Enter subreddit name again: ")
+    bVerifySubreddit = input("Are you sure? (answer True or False)\n")
+
+downloadpath = input("Where would you like to download these memes? \n")#.encode('utf-8')
+downloadpath = 'C:\\users\\pc\\documents\\buffer'
+print("The path you have chosen is: " + str(downloadpath))
+
+functions.createNewDirectoryAndChDir(downloadpath)
+
+subredditstream = functions.createSubredditInstance(reddit, subredditname)
+
+functions.mainRedditLoop(subredditstream, showDebugMessages, debugLogFileName)
+
+print('number of times looped: ' + str(loops))
+functions.os.chdir(homedirectory)
+functions.displayAnalytics(imagesqueried, imagescounted)
+functions.subredditpullcompleted()
